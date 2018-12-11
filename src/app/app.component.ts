e11f6b7c11b34c0a7cf48b4d30e316c541387c75
Nildiert <niljordan23@gmail.com>
2018-12-11 03:44:43 -0500

    ngSwitch 11122018_0344

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'platziSquare';
   lugares:any = [
-    {active: true, nombre: 'Veterinaria el becerro'},
-    {active: true, nombre: 'Veterinaria el becerro'},
-    {active: true, nombre: 'Floristeria, la pajita'},
-    {active: false, nombre: 'Sushi el gamin'},
-    {active: true, nombre: 'Hotel la bendicion'},
-    {active: false, nombre: 'Zapateria el clavo'},
+    {cercania: 1, distancia: 1, active: true, nombre: 'Veterinaria el becerro'},
+    {cercania: 1, distancia: 1.8, active: true, nombre: 'Veterinaria el becerro'},
+    {cercania: 2, distancia: 5, active: true, nombre: 'Floristeria, la pajita'},
+    {cercania: 2, distancia: 10, active: false, nombre: 'Sushi el gamin'},
+    {cercania: 3, distancia: 35, active: true, nombre: 'Hotel la bendicion'},
+    {cercania: 3, distancia: 120, active: false, nombre: 'Zapateria el clavo'},
 
   ]
   lat:number = 4.613092;
   lng:number = -74.1843277;
+
+  
   constructor(){
   }
 
